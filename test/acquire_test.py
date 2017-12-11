7860c9dcfeea70aeff32f264eaaad305213ef850
Thomas Henderson <tom@mathpunk.net>
2017-12-11 14:29:15 -0600

    Load documents into corpus

--- test/acquire_test.py
+++ test/acquire_test.py
@@ -3,9 +3,22 @@ from word_vectors.acquire import Corpus
 # document 1
 test_document = "The St. Louis plant had to close. It would die of old age. Workers had been making cars there since the onset of mass automotive production in the 1920s."
 
+
+corpus = Corpus("/home/man/dailies/saturday/word_vectors/data/en_US/en_US.news.txt")
+document = corpus.documents[1]
+
+
 def test_sanity():
     assert 1 + 1 == 2
 
+
 def test_corpus_creation():
-    corpus = Corpus("../data/en_US/en_US.news.txt")
-#     len(corpus.documents) > 0
+    len(corpus.documents) > 0
+    assert type(document) == str
+    assert document == test_document
+
+
+# def test_document():
+#     normalized = document.normalize
+#     assert len(normalized) == 29
+
--- word_vectors/acquire.py
+++ word_vectors/acquire.py
@@ -1,3 +1,28 @@
+import os
+
 class Corpus:
+    documents = []
+
     def __init__(self, path):
-        print("hello corpus")
+        file = open(path, 'r')
+        data = file.read()
+        self.documents = data.splitlines()
+
+
+# english_news = "data/en_US/en_US.news.txt"
+# import os
+
+# news_file = open(english_news, 'r')
+# data = news_file.read()
+
+# type(data)
+
+
+# class Corpus:
+#     documents = []
+
+#     def __init__(self, path):
+#         for filename in os.listdir(path):
+#             document = Document()
+#             self.documents.append(document)
+
